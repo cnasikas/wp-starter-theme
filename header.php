d2279a33b07a8a5a098b6ca66a532d4eecbc4a7c
Christos Nasikas <xristosnasikas@gmail.com>
2018-10-27 15:45:33 +0300

    Add extra header meta

--- header.php
+++ header.php
@@ -12,15 +12,17 @@ $menu = new Menu();
     <head>
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-		<base href="<?php echo home_url(); ?>" />
-		<meta name="keywords" content="">
-		<meta name="classification" content="">
-		<meta name="HandheldFriendly" content="True">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="manifest" href="site.webmanifest">
+    		<base href="<?php echo home_url(); ?>" />
+    		<meta name="keywords" content="">
+    		<meta name="classification" content="">
+		    <meta name="HandheldFriendly" content="True">
         <meta name="MobileOptimized" content="320">
         <meta name="msapplication-tap-highlight" content="no" />
-		<link rel="icon" href="<?php echo APP_IMG_URL; ?>favicon-32x32.png" sizes="32x32" />
+		    <link rel="icon" href="<?php echo APP_IMG_URL; ?>favicon-32x32.png" sizes="32x32" />
         <link rel="icon" href="<?php echo APP_IMG_URL; ?>favicon-192x192.png" sizes="192x192" />
+        <link rel="apple-touch-icon" href="<?php echo APP_IMG_URL; ?>favicon-192x192.png">
         <link rel="apple-touch-icon-precomposed" href="<?php echo APP_IMG_URL; ?>favicon-192x192.png" />
         <meta name="msapplication-TileImage" content="<?php echo APP_IMG_URL; ?>favicon-192x192.png" />
         <meta name="msapplication-TileColor" content="#FFFFFF">
